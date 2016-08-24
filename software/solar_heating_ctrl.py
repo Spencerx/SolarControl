22d39fef429d2381908a47be3b14f5a382643d1e
Christian Eisenhut (Home-Office) <christian.eisenhut@oertli-instruments.com>
2016-08-24 07:14:02 +0200

    printout mean storage temperature
    Signed-off-by: Christian Eisenhut (Home-Office) <christian.eisenhut@oertli-instruments.com>

--- software/solar_heating_ctrl.py
+++ software/solar_heating_ctrl.py
@@ -80,6 +80,7 @@ while True:
     # ========================================
     # mean-temperature:
     StorageMeanTemp = (T1 * 30 + T2 * 5 + T3 * 5 + T4 * 10 + T5 * 20 + T6 * 30) / 100
+    print("StorageMeanTemp = %2.1f" % StorageMeanTemp)
 
     if (StorageMeanTemp > 80):  # activ cooling
         SolarPumpRunning = True
