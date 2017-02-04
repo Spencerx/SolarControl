75e59a2073a386db047fbf62b230f4aab0fbf035
Christian <christian@dagabumm.ch>
2017-02-04 14:32:47 +0100

    improve temp-logging
    Signed-off-by: Christian <christian@dagabumm.ch>

--- software/scan_ds18b20_sensors.py
+++ software/scan_ds18b20_sensors.py
@@ -3,7 +3,22 @@
 from w1thermsensor import W1ThermSensor
 from time import *
 
+
+def postData(data, node):
+    url = 'http://localhost/emoncms/input/post.json?node=' + str(node) + '&json={' + data + '}
+    answer = urlopen(url)
+
+Sensors = W1ThermSensor.get_available_sensors()
+
+
 while (True):
-    for sensor in W1ThermSensor.get_available_sensors():
-        print("Sensor %s hat die Temperatur %.2f" % (sensor.id, sensor.get_temperature()))
+    for sensor in Sensors:
+        id = sensor.id
+        temp = sensor.get_temperature()
+        print("Sensor %s hat die Temperatur %.2f" % (id, temp))
+        SensorData = "%s:%.2f" % (id, temp)
+        try:
+            postData(SensorData, 1)
+        except:
+            print("posting data not successful\n")
         sleep(1)
