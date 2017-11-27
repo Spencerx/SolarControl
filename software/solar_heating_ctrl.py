1e1f8892b0f31dd40c435dbf52a12fc4f75caf4c
Christian <christian@dagabumm.ch>
2017-11-27 22:06:16 +0100

    Change night-setback parameters
    Signed-off-by: Christian <christian@dagabumm.ch>

--- software/solar_heating_ctrl.py
+++ software/solar_heating_ctrl.py
@@ -181,9 +181,9 @@ try:
         elif (HeatingMode == HeatingWithNightSetback):
             if (actual_UTC_time.tm_hour >= 20):
                 HeatTempSetpoint = 30
-            elif (actual_UTC_time.tm_hour >= 5):
+            elif (actual_UTC_time.tm_hour >= 6):
                 HeatTempSetpoint = emon.readHeatingTempSetpoint()
-            elif (actual_UTC_time.tm_hour >= 3):
+            elif (actual_UTC_time.tm_hour >= 2):
                 HeatTempSetpoint = emon.readHeatingTempSetpoint() + 10
             else:
                 HeatTempSetpoint = 30
